3b8205dcd9f46ffac4e73f7b536f933cfe7d6fd2
“doberman7” <ivanrubioangeles@gmail.com>
2020-08-03 18:06:27 -0500

    2 comit

---
+++ app_my_version/main.js
@@ -0,0 +1,13 @@
+new Vue({
+  el: '#app',
+  data: {
+    submissions: Seed.submissions
+  },
+  computed: {
+    sortedSubmissions (){
+      return this.submissions.sort((a,b) => {
+        return b.votes - a.votes
+      });
+    }
+  }
+});
